e6aee307fb14104808dd6c445a258c365273d62c
SamyazaCode <lautaropaz__@hotmail.com>
2025-02-25 14:46:04 -0300

    Mejorando SEO con metadatos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,8 +40,9 @@ const Navbar = () => {
     <nav className="navbar navbar-dark fixed-top">
       <div className="container-fluid">
         
-
-        <img className='navbar-logo' src="imgs/logo-tatto.webp" loading="lazy" alt='logo' onClick={scrollToTop}/>
+        <a href="#inicio" onClick={scrollToTop}>
+          <img className='navbar-logo' src="imgs/logo-tatto.webp" loading="lazy" alt='logo' onClick={scrollToTop}/>
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
